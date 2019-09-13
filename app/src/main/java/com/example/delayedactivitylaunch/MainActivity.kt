b455946bc3582c585a06f6b64f83694770ffb060
Carmen Alvarez <calvarez@libon.com>
2019-09-13 12:38:14 +0200

    Launch MainActivity2 from MainActivity
    * Add a button to MainActivity
    * When the button is tapped, launch MainActivity2 after 5 seconds. Show a progress bar during this delay.

--- app/src/main/java/com/example/delayedactivitylaunch/MainActivity.kt
+++ app/src/main/java/com/example/delayedactivitylaunch/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.delayedactivitylaunch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.button).setOnClickListener {
+            val progressBar = findViewById<View>(R.id.progress_bar)
+            progressBar.visibility = View.VISIBLE
+            it.postDelayed({
+                progressBar.visibility = View.GONE
+                startActivity(Intent(this, MainActivity2::class.java))
+            }, 5000)
+        }
     }
 }
